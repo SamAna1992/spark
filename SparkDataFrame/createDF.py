edf3a83492c43473a40c44b66af95a5ea32426d0
Sumit-BigData <anandjnnce10@gmail.com>
2018-01-26 16:03:36 +0530

    Creates a Spark Data Frame from an RDD

--- SparkDataFrame/createDF.py
+++ SparkDataFrame/createDF.py
@@ -3,11 +3,17 @@ from pyspark.sql import SQLContext,Row
 
 if __name__ == "__main__":
 	conf = SparkConf().setAppName("RddToDataFrame")
+	## SparkContext is the entry point for an Spark Application
 	sc = SparkContext(conf=conf)
 	sqlContext = SQLContext(sc)
-	inpRdd = sc.textFile("file:///home/sumit/Desktop/certification/hdp_spark/employee.txt")
+	## Read a file from local FS and create RDD
+	inpRdd = sc.textFile("file:///home/sumit/employee.txt")
 	splitInpRdd = inpRdd.map(lambda part: part.split(","))
+	## Convert each element of RDD into a Row 
 	createRow = splitInpRdd.map(lambda x: Row(empid = x[0], name = x[1] , dept = x[2]))
+	## Create a data frame from exisiting RDD
 	myDf = sqlContext.createDataFrame(createRow)
+	## prints the RDD
 	myDf.show()
+	## Stop the SparkContext
 	sc.stop()
